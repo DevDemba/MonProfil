9880ca5c640abab96b10add879b3604e202b498b
Demba <38429183+DevDemba@users.noreply.github.com>
2018-10-23 18:25:55 +0200

    begin commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'admin', component: AdminComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
